db84418e9742964d153d0fb03b281f4d6400f4b8
alicfamahdi <alicfamahdi@gmail.com>
2024-09-09 18:18:30 +0700

    change main

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-      
+        'npm' : '2306221056',
+        'name': 'Aliefa Alsyafiandra Setiawati Mahdi',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
